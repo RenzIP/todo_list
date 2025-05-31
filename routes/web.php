99246cdb4ffff6a9ee158b54078223a3e4747690
Bagas Agung Wiyono <zenkun.enterkill13@gmail.com>
2025-05-31 15:30:38 +0700

    push

--- routes/web.php
+++ routes/web.php
@@ -9,25 +9,25 @@ use App\Http\Middleware\CheckRole;
 
 // Public routes
 
-    Route::get('/', [AuthController::class, 'showLoginForm'])->middleware('guest');
-    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login')->middleware('guest');
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register')->middleware('guest');
-    Route::post('/register', [AuthController::class, 'register'])->name('create.user');
+Route::get('/', [AuthController::class, 'showLoginForm'])->middleware('guest');
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login')->middleware('guest');
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register')->middleware('guest');
+Route::post('/register', [AuthController::class, 'register'])->name('create.user');
 
 
 // Protected routes
 Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-    
+
     // Dashboard routes
     Route::prefix('dashboard')->group(function () {
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard')->middleware(CheckRole::class);
         // Kelola User routes
         Route::resource('users', KelolaUserController::class);
     });
-    
-    
+
+
     // User routes - Tasks CRUD
     Route::prefix('tasks')->group(function () {
         Route::get('/', [TaskController::class, 'index'])->name('tasks.index')->middleware(CheckRole::class);
